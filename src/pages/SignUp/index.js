521eff60717caf91c566f91b7755220f0a979ee1
Josué Gimenes <josuegalvares@gmail.com>
2019-12-28 13:40:19 -0300

    Initial Route

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import { Image } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
 import logo from '~/assets/logo.png';
 
@@ -83,3 +84,9 @@ export default function SignUp({ navigation }) {
     </Background>
   );
 }
+
+SignUp.propTypes = {
+  navigation: PropTypes.shape({
+    getParam: PropTypes.func,
+  }).isRequired,
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,30 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    SignUp,
-  }),
-);
+import Dashboard from './pages/Dashboard';
+
+// Aplica um navigator dentro de outro.
+// Exporta uma função que retorna o componente acessando a rota por condição.
+export default (signedIn = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        // Rotas para autenticação de cadastro
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        // Rotas para usuário logado
+        App: createBottomTabNavigator({
+          Dashboard,
+        }),
+      },
+      {
+        // Configuração de rota padrão
+        initialRouteName: signedIn ? 'App' : 'Sign',
+      },
+    ),
+  );
