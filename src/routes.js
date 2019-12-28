d850887114c60bc4ccf8cc7350d1a1e8376f9263
Josué Gimenes <josuegalvares@gmail.com>
2019-12-28 14:26:24 -0300

    Routes Stylization

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 
 import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
 
 // Aplica um navigator dentro de outro.
 // Exporta uma função que retorna o componente acessando a rota por condição.
@@ -18,9 +19,22 @@ export default (signedIn = false) =>
           SignUp,
         }),
         // Rotas para usuário logado
-        App: createBottomTabNavigator({
-          Dashboard,
-        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#FFF',
+              inactiveTintColor: 'rgba(255, 255, 255, 0.6)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          },
+        ),
       },
       {
         // Configuração de rota padrão
