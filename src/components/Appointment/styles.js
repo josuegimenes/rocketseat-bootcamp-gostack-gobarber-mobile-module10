823d8e5a789721aff3a6700c0034c02aef1fff66
Josué Gimenes <josuegalvares@gmail.com>
2019-12-30 16:24:38 -0300

    Api Schedules

--- src/components/Appointment/styles.js
+++ src/components/Appointment/styles.js
@@ -9,7 +9,9 @@ export const Container = styled.View`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+
+  opacity: ${props => (props.past ? 0.6 : 1)};
 `;
 
 export const Left = styled.View`
